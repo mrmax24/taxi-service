28c3ab4989a9f11d3ef57ccf9594fb3cd6cc5868
Maks Hrytsyna <101352509+mrmax24@users.noreply.github.com>
2023-01-31 03:33:55 +0200

    Update ConnectionUtil.java

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String URL =
-            "mysql://bc90459fde4a73:63f814ad@us-cdbr-east-06.cleardb.net/heroku_bb1c62b283b50d2?reconnect=true";
+            "jdbc:mysql://bc90459fde4a73:63f814ad@us-cdbr-east-06.cleardb.net/heroku_bb1c62b283b50d2?reconnect=true";
     private static final String USERNAME = "bc90459fde4a73";
     private static final String PASSWORD = "63f814ad";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
